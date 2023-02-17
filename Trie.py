306637c9211abdc3f991444459cd2940f2540eb7
Matheus <matheustae@hotmail.com>
2023-02-17 17:28:14 -0300

    começando

---
+++ Trie.py
@@ -0,0 +1,25 @@
+class No:
+    def __init__(self):
+        self.finalPalavra = False
+        self.filhos = []
+        for i in range(0, 30):
+	        self.filhos.append(None)
+
+def mapeiaCharParaIndice(char):
+    # ord() retorna o valor na tabela ASCI
+    return (ord(char) - ord('a'))
+
+def insert( raiz, chave):
+    no = raiz
+    for letra in chave:
+        i = mapeiaCharParaIndice(letra)
+        if(not no.filhos[i]):
+            no.filhos[i] = No()
+        no = no.filhos[i]
+    no.finalPalavra = True
+    return no
+
+raiz = No()
+raiz = insert(raiz,'preto')
+print(raiz[0])
+
